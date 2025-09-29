f5c22ec7bb2968601447845223650d593c0b8075
jancurevic <marcin.janczurewicz@in-team.com.pl>
2025-09-29 22:16:54 +0200

    Fix React Router basename for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <BrowserRouter basename="/humanity-art">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
