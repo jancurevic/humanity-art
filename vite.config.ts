90d6cb9d24171eaa4fe12b391ad49c4848377d94
jancurevic <marcin.janczurewicz@in-team.com.pl>
2025-09-30 20:48:53 +0200

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/humanity-art/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
