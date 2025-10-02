e0e9f5151bd2e77b4fdec6a342bf11016c37f9f6
jancurevic <marcin.janczurewicz@in-team.com.pl>
2025-10-02 13:55:11 +0200

    change exhibition dates

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -84,7 +84,7 @@ const Index = () => {
                   <svg className="w-5 h-5 text-gray-300" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                   </svg>
-                  <span className="font-medium text-gray-300">Friday, 04.10.2025</span>
+                  <span className="font-medium text-gray-300">Saturday, 04.10.2025</span>
                   <span className="text-sm text-gray-300">•</span>
                   <svg className="w-4 h-4 text-gray-300" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -95,7 +95,7 @@ const Index = () => {
                   <svg className="w-5 h-5 text-gray-300" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                   </svg>
-                  <span className="font-medium text-gray-300">Saturday, 05.10.2025</span>
+                  <span className="font-medium text-gray-300">Sunday, 05.10.2025</span>
                   <span className="text-sm text-gray-300">•</span>
                   <svg className="w-4 h-4 text-gray-300" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
